8d9b5fcd66f9b75092a39a39d1cdb50d1ee4ea7e
yagnik26 <yagnikgoti26@gmail.com>
2023-10-09 11:06:31 +0530

    update response

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -23,7 +23,8 @@ const signup = async (req, res) => {
     });
     const token = genrateToken(data._id, data.email);
     return res.status(200).json({
-      data: token,
+      status: "success",
+      token: token,
     });
   } catch (error) {
     console.log(error);
@@ -41,21 +42,28 @@ const login = async (req, res) => {
     }
     const emailChack = await user.findOne({ email: email });
     if (!emailChack) {
-      return res.status(500).json("User not found");
+      return res
+        .status(500)
+        .json({ status: "failed", message: "User not found" });
     }
     const matchPassword = bcrypt.compare(password, emailChack, password);
     if (!matchPassword) {
-      return res.status(500).json("Password Incorrect !");
+      return res.status(500).json({
+        status: "failed",
+        message: "Password Incorrect !",
+      });
     }
     const token = genrateToken(emailChack._id, email);
     return res.status(200).json({
+      status: "success",
       token: token,
     });
   } catch (error) {
     console.log(error);
-    return res
-      .status(500)
-      .json("Internal server error please try after some time!");
+    return res.status(500).json({
+      status: "failed",
+      message: "Internal server error please try after some time!",
+    });
   }
 };
 
@@ -65,17 +73,22 @@ const allUsers = async (req, res) => {
     const tokenVarify = verifyToken(token);
     const userChack = await user.findOne({ _id: tokenVarify.id });
     if (!userChack) {
-      return res.status(500).json("token Invalid");
+      return res.status(500).json({
+        status: "faield",
+        message: "token Invalid",
+      });
     }
     const allUser = await user.find();
     return res.status(200).json({
+      status: "success",
       result: allUser,
     });
   } catch (error) {
     console.log(error);
-    return res
-      .status(500)
-      .json("Internal server error please try after some time!");
+    return res.status(500).json({
+      status: "faield",
+      message: "Internal server error please try after some time!",
+    });
   }
 };
 
@@ -85,27 +98,38 @@ const update = async (req, res) => {
     const tokenVarify = verifyToken(token);
     const { firstName, lastName, DOB, mobile, email } = req.body;
     if (!(firstName, lastName, DOB, mobile, email)) {
-      return res.status(400).json("All field are required");
+      return res
+        .status(400)
+        .json({ status: "faield", message: "All field are required" });
     }
     const userChack = await user.findOne({ _id: tokenVarify.id });
     if (userChack.email !== tokenVarify.email) {
-      return res.status(500).json("do not update user");
+      return res
+        .status(500)
+        .json({ status: "faield", message: "do not update user" });
     }
-    const updateUser = await user.findByIdAndUpdate({_id: tokenVarify.id} , {
-      firstName,
-      lastName,
-      DOB,
-      mobile,
-      email,
-    })
+    const updateUser = await user.findByIdAndUpdate(
+      { _id: tokenVarify.id },
+      {
+        firstName,
+        lastName,
+        DOB,
+        mobile,
+        email,
+      }
+    );
+    const tokenGenrate = genrateToken(updateUser._id, updateUser.email);
     return res.status(200).json({
-      message: "User updated successfully"
-    })
+      status: "success",
+      message: "User updated successfully",
+      token: tokenGenrate,
+    });
   } catch (error) {
     console.log(error);
-    return res
-      .status(500)
-      .json("Internal server error please try after some time!");
+    return res.status(500).json({
+      status: "faield",
+      message: "Internal server error please try after some time!",
+    });
   }
 };
 

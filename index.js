48faea2add188a72f308a2642af89e91f996f13b
yagnik26 <yagnikgoti26@gmail.com>
2023-10-10 19:21:07 +0530

    add cors

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const connect = require('./config/db');
+const express = require("express");
+const connect = require("./config/db");
 require("dotenv").config();
-const userRouter = require("./routes/userRoute.js")
+const cors = require("cors");
+const userRouter = require("./routes/userRoute.js");
 const app = express();
 app.use(express.json());
 
-app.use('/api/user/',userRouter)
+app.use(cors());
+app.use("/api/user/", userRouter);
 
 app.listen(process.env.PORT, () => {
-    connect()
-    console.log('listening on port ' + process.env.PORT);
-})
+  connect();
+  console.log("listening on port " + process.env.PORT);
+});
